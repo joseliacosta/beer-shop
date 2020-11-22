1b053045eff8e78f70801b2c05d7ec6718546748
Josélia L. Costa <joselialcosta@gmail.com>
2020-11-22 20:10:24 -0300

    feat(main): creates main component to wrapper searchfield

---
+++ app/components/Main/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  padding: 0 1rem;
+  background: #f9c104;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 90vh;
+  flex: 0 0 100%;
+`
